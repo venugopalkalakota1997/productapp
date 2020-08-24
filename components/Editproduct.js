2dcf51d6fa51d97d8332b0b73bde1882481ba687
venugopalreddykalakota1997 <venugopalreddykalakota1997@gmail.com>
2020-08-24 09:41:33 +0530

    max

--- components/Editproduct.js
+++ components/Editproduct.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { globalstyles } from "../globalstyles/globalstyles";
 import {Image, Button,View, Picker, Text, TouchableOpacity,ActivityIndicator, ScrollView, TextInput } from "react-native";
 import axios from 'axios'
@@ -6,18 +6,24 @@ import { useNavigation } from '@react-navigation/native';
 
 
 export default function EditProductComponent(props) {
-    const detail = props.route.params.item
+    let {detail }= props.route.params.item
     const navigation = useNavigation();
     let [newproduct, setproduct] = useState({
-        id: detail.id,
-        name: detail.name,
-        productdetails: detail.productdetails,
-        quantity: detail.quantity,
-        price: detail.price,
-        category: detail.category,
-        image: detail.image
+        id: 0,
+        name: '',
+        productdetails: '',
+        quantity: '',
+        price: '',
+        category: '',
+        image: ''
 
     });
+    useEffect(() => {
+        console.log(props.route.params.item);
+        setproduct(props.route.params.item)
+        console.log(newproduct);
+    })
+    
     let [buttonstatus,setbuttonstatus]=useState({buttonstatus:false})
  
     function namechange(event){       
@@ -89,16 +95,16 @@ export default function EditProductComponent(props) {
 <View style={globalstyles.addform}>
                 <Text style={globalstyles.texttitle}>Edit Product</Text>
                 <Text>Product Name: <Text style={{color:'red'}}>*</Text></Text>
-                <TextInput style={globalstyles.forminput} name="name" onChangeText={namechange} value={detail.name}></TextInput>
+                <TextInput style={globalstyles.forminput} name="name" onChangeText={namechange} value={newproduct['name']}></TextInput>
                 <Text>Product Description: <Text style={{color:'red'}}>*</Text></Text>
-                <TextInput style={globalstyles.forminput} name="productdetails" numberOfLines={5} multiline={true} onChangeText={detailschange} value={detail.productdetails}/>
+                <TextInput style={globalstyles.forminput} name="productdetails" numberOfLines={5} multiline={true} onChangeText={detailschange} value={newproduct['productdetails']}/>
                 <Text>Product Price: <Text style={{color:'red'}}>*</Text></Text>
-                <TextInput style={globalstyles.forminput}name="price"  keyboardType={'number-pad'} onChangeText={pricechange} value={detail.price}/>
+                <TextInput style={globalstyles.forminput}name="price"  keyboardType={'number-pad'} onChangeText={pricechange} value={newproduct['price']}/>
                 <Text>Product Quantity: <Text style={{color:'red'}}>*</Text></Text>
-                <TextInput style={globalstyles.forminput} name="quantity" keyboardType={'number-pad'} onChangeText={quantitychange} value={detail.quantity}/>
+                <TextInput style={globalstyles.forminput} name="quantity" keyboardType={'number-pad'} onChangeText={quantitychange} value={newproduct['quantity']}/>
                 <Text>Product Category: <Text style={{color:'red'}}>*</Text></Text>
                 <Picker style={{ height: 50, width: 300 }} style={globalstyles.forminput} onValueChange={selectcategory} >
-                    <Picker.Item label={detail.category}  value={detail.category} />
+                    <Picker.Item label={newproduct['category']}  value={newproduct['category']} />
                     <Picker.Item label="Televison" name="Televison" value="Televison" />
                     <Picker.Item label="Mobile" name="Mobile" value="Mobile" />
                     <Picker.Item label="Furniture" name="Furniture" value="Furniture" />
@@ -107,7 +113,7 @@ export default function EditProductComponent(props) {
                 <Text>Product Image: <Text style={{color:'red'}}>*</Text></Text>
                 <Picker style={{ height: 50, width: 300 }}   style={globalstyles.forminput} onValueChange={selectimage}>
                    
-                    <Picker.Item label={detail.image} value={detail.image} />
+                    <Picker.Item label={newproduct['image']} value={newproduct['image']} />
                     <Picker.Item label="Televison" name="Televison" value="televison.jpg" />
                     <Picker.Item label="Mobile" name="Mobile" value="mobile.jpg" />
                     <Picker.Item label="Furniture" name="Furniture" value="furniture.jpg" />
